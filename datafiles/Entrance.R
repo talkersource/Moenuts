3b0e72865ba6a8256ea29db72012df932d12c93d
Mike Irving (MoeSoft Developments) <>
2017-01-07 06:25:13 +0000

    Moenuts 1.4.0

---
+++ datafiles/Entrance.R
@@ -0,0 +1,6 @@
+You walk in the room and everyone stops and stares.  You feel like a
+thousand eyes are stareing at you.  All of a sudden, you hear someone
+whisper.  Is that $N ?  Is it really them?  What is $N doing
+here?  Suddenly someone comes upto you and pats you on the back...
+$N !  How ya been buddy?  So glad you came to join the party!
+Grab yourself a seat!  Stay a while and enjoy the fun!
